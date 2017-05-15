fcef5bc568e489c6fe7b1288da3ec26245cfc8e2
yli127 <1542970020@qq.com>
2017-05-14 19:03:03 -0700

    5.15  EF day 1

---
+++ chiookSolution/ChiookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChiookSystem.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        [key]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+    //Navigation properties for use by LINQ
+    public virtual ICollection<Album> Albums { get; set; }
+}
---
+++ chiookSolution/ChiookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+#endregion
+
+namespace ChiookSystem.DAL
+{
+    //This is an interanl class for security reasons
+    //Access is restricted tio within this class library project
+    //Inherits DbContext for Entity Framework,which requires
+    //  System.Data.Entity
+    internal class ChinookContext: DbContext
+    {
+        // Pass the connection string name to the 
+        //DbContext using: base()
+        // i.e. connection string name is "ChinookDB"
+        public ChinookContext():base("ChinookDB")
+        {
+
+        }
+    }
+}
